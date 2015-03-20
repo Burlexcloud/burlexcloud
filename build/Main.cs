1a8a596532e3f72f208a0184c01db1ca45196cf8
Burlexcloud <tom@burlexcloud.ru>
2015-03-20 23:54:54 +0300

    Update structure and add about box

---
+++ build/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace build
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AboutBox AB = new AboutBox();
+            AB.Show();
+        }
+    }
+}
